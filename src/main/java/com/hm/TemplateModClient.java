785e42f82c4e70bee9e50b7cc78987339729f092
lightty <sijielove1@qq.com>
2023-10-17 10:49:25 +0800

    Initial commit

---
+++ src/main/java/com/hm/TemplateModClient.java
@@ -0,0 +1,10 @@
+package com.hm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
